ed94605e0f4d509e21ffb51bb082ea436e86bd8f
Amruth-S-S <amruthss45@gmail.com>
2026-07-16 12:49:00 +0530

    Update PackagesContent.jsx

--- app/Packages/PackagesContent.jsx
+++ app/Packages/PackagesContent.jsx
@@ -170,7 +170,7 @@ const packages = [
     duration: "5 Days 4 Nights",
     group: "Max 25 People",
     highlight: "Best Value",
-    image: "/Images/THAILAND.jpeg",
+    image: "/Images/1.png",
     color: "from-teal-500 to-cyan-600",
     places: ["Pattaya", "Bangkok", "Coral Island", "Sanctuary of Truth"],
     inclusions: [
